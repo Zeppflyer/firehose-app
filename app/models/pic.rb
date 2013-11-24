b6ce7862707a23ad20234188393db8bfed378797
Sean <sean@sean-VirtualBox.(none)>
2013-11-24 10:36:18 -0500

    added validations

--- app/models/pic.rb
+++ app/models/pic.rb
@@ -2,5 +2,9 @@ class Pic < ActiveRecord::Base
 
 	mount_uploader :image, ImageUploader
 
-
+	validates :lesson, :presence => true, :numericality =>  
+	{ :less_than_or_equal_to =>100}
+	
+	validates :learned, :presence => true
+	validates :learned, :presence => true
 end
